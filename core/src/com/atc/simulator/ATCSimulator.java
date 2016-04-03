a3294c312c1eaa0fbf941418eff8a7672829777b
Luke Frisken <9505628@student.swin.edu.au>
2016-04-03 11:42:31 +1000

    figured out how to link the assets in android studio

--- core/src/com/atc/simulator/ATCSimulator.java
+++ core/src/com/atc/simulator/ATCSimulator.java
@@ -13,7 +13,7 @@ public class ATCSimulator extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("assets/badlogic.jpg");
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
