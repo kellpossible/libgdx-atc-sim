ea4ce20e5eed799e7c06d4531eefe635066a07bc
Luke Frisken <9505628@student.swin.edu.au>
2016-04-03 14:08:04 +1000

    3d globe display

--- core/src/com/atc/simulator/ATCSimulator.java
+++ core/src/com/atc/simulator/ATCSimulator.java
@@ -2,26 +2,83 @@ package com.atc.simulator;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 public class ATCSimulator extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	public PerspectiveCamera cam;
+
+	public Model model;
+	public ModelInstance instance;
+	public ModelBatch modelBatch;
+    public Environment environment;
+	public CameraInputController camController;
+
 	@Override
 	public void create () {
+        modelBatch = new ModelBatch();
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-	}
+
+		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getWidth());
+		cam.position.set(10f, 10f, 10f);
+		cam.lookAt(0, 0, 0);
+		cam.near = 1f;
+		cam.far = 300f;
+		cam.update();
+
+		ModelBuilder modelBuilder = new ModelBuilder();
+		model = modelBuilder.createSphere(5f, 5f, 5f, 32, 32,
+				new Material(ColorAttribute.createDiffuse(Color.BLUE)),
+				VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+
+		instance = new ModelInstance(model);
+
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+        camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
+    }
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        camController.update();
+		modelBatch.begin(cam);
+		modelBatch.render(instance, environment);
+		modelBatch.end();
 	}
+
+	@Override
+	public void dispose () {
+		modelBatch.dispose();
+		model.dispose();
+	}
+
+    @Override
+    public void resize(int width, int height)
+    {
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getWidth());
+        camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
+    }
 }
