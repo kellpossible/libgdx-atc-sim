a56040f8c7c984c57faf88b19a2057e23f338e80
Luke Frisken <9505628@student.swin.edu.au>
2016-04-03 11:29:15 +1000

    added libgdx gradle project

---
+++ desktop/src/com/atc/simulator/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.atc.simulator.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.atc.simulator.ATCSimulator;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ATCSimulator(), config);
+	}
+}
