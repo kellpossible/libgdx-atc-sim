d8708c86411f2776e64850e020e418ef3b089583
Uros <7671725@student.swin.edu.au>
2016-05-26 14:29:54 +1000

    jsonconfig.java

--- core/src/com/atc/simulator/Config/JSONConfig.java
+++ core/src/com/atc/simulator/Config/JSONConfig.java
@@ -1,34 +1,34 @@
-package com.atc.simulator.Config;
-
-
-import java.util.HashMap;
-import
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-/**
- * Created by amiritis on 25/05/2016.
- */
-public class JSONConfig
-{
-    HashMap<String, String> PortList = new HashMap<String, String>();
-    public JSONConfig(String aFileName)
-//    {
-//        JSONParser parser = new JSONParser();
-//
-//        try
-//        {
-//            Object data = parser.parse( new FileReader(aFileName) );
-//            JSONObject jObject = (JSONObject) data;
-//
-//        }
-//        catch (Exception e)
-//        {
-//            e.printStackTrace();
-//        }
+//package com.atc.simulator.Config;
 //
 //
-//    }
-}
+////import java.util.HashMap;
+////import
+////import org.json.simple.JSONArray;
+////import org.json.simple.JSONObject;
+////import org.json.simple.parser.JSONParser;
+////import org.json.simple.parser.ParseException;
+//
+///**
+// * Created by amiritis on 25/05/2016.
+// */
+//public class JSONConfig
+//{
+//    HashMap<String, String> PortList = new HashMap<String, String>();
+//    public JSONConfig(String aFileName)
+////    {
+////        JSONParser parser = new JSONParser();
+////
+////        try
+////        {
+////            Object data = parser.parse( new FileReader(aFileName) );
+////            JSONObject jObject = (JSONObject) data;
+////
+////        }
+////        catch (Exception e)
+////        {
+////            e.printStackTrace();
+////        }
+////
+////
+////    }
+//}
