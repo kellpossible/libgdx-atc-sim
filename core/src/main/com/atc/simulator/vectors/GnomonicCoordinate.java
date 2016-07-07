00d6d8acb5866f08a2e5598a9eaeead4ab3f3f09
Luke Frisken <l.frisken@gmail.com>
2016-07-07 14:46:09 +1000

    Refactoring and more work on #92

---
+++ core/src/main/com/atc/simulator/vectors/GnomonicCoordinate.java
@@ -0,0 +1,33 @@
+package com.atc.simulator.vectors;
+
+import pythagoras.d.Vector3;
+
+/**
+ * Created by luke on 7/07/16.
+ *
+ * Represents a coordinate in the gnomonic projection
+ * https://en.wikipedia.org/wiki/Gnomonic_projection
+ *  *
+ * @author Luke Frisken
+ */
+public class GnomonicCoordinate extends Vector3 {
+    private GeographicCoordinate projectionReference;
+    /**
+     * Convert a GeographicCoordinate into a GnomonicCoordinate based on its projection reference
+     * @param geographicCoordinate the coordinate we are converting
+     * @param projectionReference the reference point of the gnomonic coordinate system
+     */
+    public GnomonicCoordinate(GeographicCoordinate geographicCoordinate, GeographicCoordinate projectionReference)
+    {
+        this.projectionReference = projectionReference;
+    }
+
+    /**
+     * Method getProjectionReference returns the projectionReference of this GnomonicCoordinate object.
+     *
+     * @return the projectionReference (type GeographicCoordinate) of this GnomonicCoordinate object.
+     */
+    public GeographicCoordinate getProjectionReference() {
+        return projectionReference;
+    }
+}
--- core/src/main/com/atc/simulator/vectors/StereographicCoordinate.java
+++ core/src/main/com/atc/simulator/vectors/StereographicCoordinate.java
@@ -4,17 +4,31 @@ import pythagoras.d.Vector3;
 
 /**
  * Created by luke on 7/07/16.
+ *
+ * Represents a coordinate in the stereographic projection:
+ * https://en.wikipedia.org/wiki/Stereographic_projection
+ *
+ * @author Luke Frisken
  */
 public class StereographicCoordinate extends Vector3 {
     private GeographicCoordinate projectionReference;
 
     /**
-     * Convert a GeographicCoordinate into a gnomic coordinate
-     * @param geographicCoordinate
-     * @param projectionReference the reference point of the gnomonic coordinate system
+     * Convert a GeographicCoordinate into a StereographicCoordinate based on its projection reference
+     * @param geographicCoordinate the coordinate to be converted
+     * @param projectionReference the reference point of the stereographic coordinate system
      */
     public StereographicCoordinate(GeographicCoordinate geographicCoordinate, GeographicCoordinate projectionReference)
     {
+        this.projectionReference = projectionReference;
+    }
 
+    /**
+     * Method getProjectionReference returns the projectionReference of this StereographicCoordinate object
+     *
+     * @return the projectionReference (type GeographicCoordinate) of this StereographicCoordinate object.
+     */
+    public GeographicCoordinate getProjectionReference() {
+        return projectionReference;
     }
 }
