b4ec91c7788263976b62e3ebeda428b46b008e98
Luke Frisken <l.frisken@gmail.com>
2016-07-12 12:11:11 +1000

    more work on gnomonic coordinate transform

--- core/src/main/com/atc/simulator/vectors/GnomonicCoordinate.java
+++ core/src/main/com/atc/simulator/vectors/GnomonicCoordinate.java
@@ -1,5 +1,7 @@
 package com.atc.simulator.vectors;
 
+import pythagoras.d.Plane;
+import pythagoras.d.Ray3;
 import pythagoras.d.Vector3;
 
 /**
@@ -12,6 +14,9 @@ import pythagoras.d.Vector3;
  */
 public class GnomonicCoordinate extends Vector3 {
     private GeographicCoordinate projectionReference;
+
+
+
     /**
      * Convert a GeographicCoordinate into a GnomonicCoordinate based on its projection reference
      * @param geographicCoordinate the coordinate we are converting
@@ -20,6 +25,31 @@ public class GnomonicCoordinate extends Vector3 {
     public GnomonicCoordinate(GeographicCoordinate geographicCoordinate, GeographicCoordinate projectionReference)
     {
         this.projectionReference = projectionReference;
+        this.z = geographicCoordinate.getAltitude();
+
+        Vector3 projectionNormal = projectionReference.getCartesian().normalize();
+        Vector3 projectionNorthNormal = projectionReference.add(0, 0, 0.001);
+        Vector3 zeroVector = new Vector3(0,0,0);
+
+        Ray3 projectionNorthNormalRay = new Ray3(zeroVector, projectionNorthNormal);
+        Ray3 projectionNormalRay = new Ray3(zeroVector, projectionNormal);
+
+        double projectionD = GeographicCoordinate.EARTH_MSL_RADIUS;
+
+        Plane projectionPlane = new Plane(projectionNormal, projectionD);
+
+        Vector3 cartesianPlaneOrigin = new Vector3();
+        projectionPlane.intersection(projectionNormalRay, cartesianPlaneOrigin);
+
+        Vector3 cartesianPlaneNorth = new Vector3();
+        projectionPlane.intersection(projectionNorthNormalRay, cartesianPlaneNorth);
+
+        Vector3 northNormal = cartesianPlaneOrigin.subtract(cartesianPlaneNorth).normalize();
+
+        //not sure if this is east or west (depends on cross product direction)
+        Vector3 eastNormal = northNormal.cross(projectionNormal);
+
+
     }
 
     /**
