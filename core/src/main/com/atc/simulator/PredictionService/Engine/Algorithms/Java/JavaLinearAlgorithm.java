286e0c24c37662e49a104392779b9d727f4b1e0d
Luke Frisken <l.frisken@gmail.com>
2016-07-20 15:13:56 +1000

    Fixed linear algorithm to use angular velocity

--- core/src/main/com/atc/simulator/PredictionService/Engine/Algorithms/Java/JavaLinearAlgorithm.java
+++ core/src/main/com/atc/simulator/PredictionService/Engine/Algorithms/Java/JavaLinearAlgorithm.java
@@ -5,12 +5,22 @@ import com.atc.simulator.flightdata.Prediction;
 import com.atc.simulator.flightdata.Track;
 import com.atc.simulator.vectors.GeographicCoordinate;
 import com.atc.simulator.vectors.SphericalVelocity;
+import pythagoras.d.Matrix3;
 import pythagoras.d.Vector3;
 
 import java.util.ArrayList;
 
 /**
  * Created by luke on 6/07/16.
+ *
+ * Predicts position based on linear angular velocity
+ * and rotating the position vector using that angular
+ * velocity.
+ *
+ *
+ * If we are to introduce a different shape than a sphere,
+ * this will need to use some sort of numeric integration
+ * which tries to keep the cartesian velocity constant.
  */
 public class JavaLinearAlgorithm extends JavaPredictionAlgorithm {
     /** @see JavaPredictionAlgorithm#makePrediction(Track) */
@@ -19,7 +29,14 @@ public class JavaLinearAlgorithm extends JavaPredictionAlgorithm {
         AircraftState state = aircraftTrack.getLatest();
         long startTime = state.getTime();
         GeographicCoordinate position = state.getPosition();
+        Vector3 cartesianPosition = position.getCartesian();
         SphericalVelocity velocity = state.getVelocity();
+
+
+        Vector3 angularVelocity = velocity.getCartesianAngularVelocity(position);
+        double dangle = angularVelocity.length();
+        Vector3 angleAxis = angularVelocity.normalize();
+
         ArrayList<AircraftState> predictedStates = new ArrayList<AircraftState>();
 
         int dt = 5000;
@@ -30,10 +47,11 @@ public class JavaLinearAlgorithm extends JavaPredictionAlgorithm {
         {
             totalDT += dt;
 
+            Matrix3 rotation = new Matrix3().setToRotation(dangle*totalDT/1000, angleAxis);
+            Vector3 cartesianPredictedPosition = rotation.transform(cartesianPosition);
+
             //make a linear prediction based on the current velocity
-            GeographicCoordinate predictedPosition = new GeographicCoordinate(
-                    position.add(velocity.mult(totalDT/1000))
-            );
+            GeographicCoordinate predictedPosition = GeographicCoordinate.fromCartesian(cartesianPredictedPosition);
 
             AircraftState predictedState = new AircraftState(
                     state.getAircraftID(),
