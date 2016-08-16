a4328fb5c30ee805e81aa6a5f591c935c60a958c
Luke Frisken <l.frisken@gmail.com>
2016-08-16 17:37:23 +1000

    research into circle and line fitting maths/algorithms. Updated worklog

--- core/src/test/com/atc/simulator/vectors/TestGraphLineSolver.java
+++ core/src/test/com/atc/simulator/vectors/TestGraphLineSolver.java
@@ -16,27 +16,34 @@ import java.util.ArrayList;
 public class TestGraphLineSolver {
     public static void main(String[] arg)
     {
-        GeographicCoordinate p1 = GeographicCoordinate.fromDegrees(0, -37.8342164,144.8746305);
-        GeographicCoordinate p2 = GeographicCoordinate.fromDegrees(0, -37.8205219,144.9055724);
-        GeographicCoordinate p3 = GeographicCoordinate.fromDegrees(0, -37.8094357,144.9322658);
-        GeographicCoordinate p4 = GeographicCoordinate.fromDegrees(0, -37.8014675,144.9607615);
-        GeographicCoordinate p5 = GeographicCoordinate.fromDegrees(0, -37.7927862,144.9880128);
-        GeographicCoordinate p6 = GeographicCoordinate.fromDegrees(0, -37.7901409,145.0093846);
         GeographicCoordinate reference = GeographicCoordinate.fromDegrees(0, -37.811151, 144.963714);
 
         GnomonicProjection projection = new GnomonicProjection(reference);
 
-        ArrayList<Vector3> points = new ArrayList<Vector3>();
-        points.add(projection.transformPositionTo(p1));
-        points.add(projection.transformPositionTo(p2));
-        points.add(projection.transformPositionTo(p3));
-        points.add(projection.transformPositionTo(p4));
-        points.add(projection.transformPositionTo(p5));
-        points.add(projection.transformPositionTo(p6));
-        Line fit = LineSolver.LeastSquaresFit(points);
+        ArrayList<Vector3> data1 = new ArrayList<Vector3>();
+        data1.add(projection.transformPositionTo(GeographicCoordinate.fromDegrees(0, -37.8342164,144.8746305)));
+        data1.add(projection.transformPositionTo(GeographicCoordinate.fromDegrees(0, -37.8205219,144.9055724)));
+        data1.add(projection.transformPositionTo(GeographicCoordinate.fromDegrees(0, -37.8094357,144.9322658)));
+        data1.add(projection.transformPositionTo(GeographicCoordinate.fromDegrees(0, -37.8014675,144.9607615)));
+        data1.add(projection.transformPositionTo(GeographicCoordinate.fromDegrees(0, -37.7927862,144.9880128)));
+        data1.add(projection.transformPositionTo(GeographicCoordinate.fromDegrees(0, -37.7901409,145.0093846)));
+
+        ArrayList<Vector3> data2 = new ArrayList<Vector3>();
+        data2.add(projection.transformPositionTo(GeographicCoordinate.fromDegrees(0, -37.729742, 144.932598)));
+        data2.add(projection.transformPositionTo(GeographicCoordinate.fromDegrees(0, -37.728076, 144.941279)));
+        data2.add(projection.transformPositionTo(GeographicCoordinate.fromDegrees(0, -37.720362, 144.948843)));
+        data2.add(projection.transformPositionTo(GeographicCoordinate.fromDegrees(0, -37.714653, 144.949926)));
+
+
+        for(Vector3 point: data2)
+        {
+            System.out.println("[" + point.x + ", " + point.y + "]");
+        }
+
+        Line fit = LineSolver.LeastSquaresFit(data1);
 
         ArrayList<Vector3> positions = new ArrayList<Vector3>();
-        positions.addAll(points);
+        positions.addAll(data1);
         positions.add(new Vector3(fit.x1, fit.y1, 0));
         positions.add(new Vector3(fit.x2, fit.y2, 0));
         
