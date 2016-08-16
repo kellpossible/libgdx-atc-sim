7b3d593a5bf6499594dd720d29fa8ccbaf79706e
Luke Frisken <l.frisken@gmail.com>
2016-08-16 13:27:00 +1000

    add dependency, and work on least squares solver

--- core/src/main/com/atc/simulator/vectors/LineSolver.java
+++ core/src/main/com/atc/simulator/vectors/LineSolver.java
@@ -1,5 +1,6 @@
 package com.atc.simulator.vectors;
 
+import org.ejml.simple.SimpleMatrix;
 import org.orangepalantir.leastsquares.Fitter;
 import org.orangepalantir.leastsquares.Function;
 import org.orangepalantir.leastsquares.fitters.LinearFitter;
@@ -20,25 +21,24 @@ import java.util.List;
 public class LineSolver {
     public static Line LeastSquaresFit(List<Vector3> points)
     {
-        Vector3 p0 = points.get(0);
-        Vector3 pend = points.get(points.size()-1);
+        final Vector3 p0 = points.get(0);
+        final Vector3 pend = points.get(points.size()-1);
         Line beta = new Line(p0.x, p0.y, pend.x, pend.y);
 
         Function function = new Function() {
             @Override
             public double evaluate(double[] values, double[] parameters) {
                 double m = parameters[0];
-                double c = parameters[1];
                 double x = values[0];
                 System.out.println(x);
-                return m*x + c;
+                return m*(x - pend.x) + pend.y;
 
 
             }
 
             @Override
             public int getNParameters() {
-                return 2;
+                return 1;
             }
 
             @Override
@@ -62,11 +62,10 @@ public class LineSolver {
 
 
         double m = (beta.y2-beta.y1)/(beta.x2-beta.x1);
-        double c = beta.y1 - beta.x1*m;
 
 
 
-        fitter.setParameters(new double[]{m,c});
+        fitter.setParameters(new double[]{m});
 
         double[] parameters = fitter.getParameters();
 
@@ -80,10 +79,51 @@ public class LineSolver {
 
 
         double newM = parameters[0];
-        double newC = parameters[1];
 
-        return new Line(p0.x, p0.x * newM + newC,
-                pend.x, pend.x*newM + newC);
+        return new Line(p0.x, newM * (p0.x - pend.x) + pend.y,
+                pend.x, newM * (pend.x - pend.x) + pend.y);
 
+    }
+
+    public static Line LeastSquaresFit2(List<Vector3> points) {
+        final Vector3 p0 = points.get(0);
+        final Vector3 pend = points.get(points.size() - 1);
+        Line beta = new Line(p0.x, p0.y, pend.x, pend.y);
+
+        int m, n;
+
+        m = points.size();
+        n = 2;
+
+        double[][] xinit = new double[m][n];
+
+
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if (j == 0)
+                {
+                    xinit[i][j] = 1;
+                }
+                if (j == 1)
+                {
+                    xinit[i][j] = points.get(i).x;
+                }
+            }
+        }
+
+        SimpleMatrix X = new SimpleMatrix(xinit);
+
+        double[][] yinit = new double[m][1];
+
+        for(int i = 0; i < m; i++)
+        {
+            yinit[i][0] = points.get(i).y;
+        }
+
+        SimpleMatrix Y = new SimpleMatrix(yinit);
+
+        return null;
     }
 }
