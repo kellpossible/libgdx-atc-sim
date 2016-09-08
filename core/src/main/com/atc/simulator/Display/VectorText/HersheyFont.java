cc6221274d6dc73cf2cab3e57b2c37a7dfc85809
Luke Frisken <l.frisken@gmail.com>
2016-09-08 18:12:10 +1000

    more work on hershey fonts

--- core/src/main/com/atc/simulator/Display/VectorText/HersheyFont.java
+++ core/src/main/com/atc/simulator/Display/VectorText/HersheyFont.java
@@ -1,9 +1,7 @@
 package com.atc.simulator.Display.VectorText;
 
-import com.google.common.collect.ContiguousSet;
-import com.google.common.collect.DiscreteDomain;
-import com.google.common.collect.Range;
-import com.google.common.primitives.Ints;
+import com.atc.simulator.flightdata.SystemState;
+import com.badlogic.gdx.math.Vector2;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 
@@ -11,7 +9,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static com.atc.simulator.Display.VectorText.HersheyFont.CharacterSet.SIMPLEX;
@@ -34,58 +31,64 @@ public class HersheyFont {
             return "Hershey " + charcode;
         }
 
-        public float[][][] linesToFloat()
+        public Vector2[][] linesToVector()
         {
-            float size = right - left;
+            float size = getSize();
 
-            float[][][] newLines = new float[lines.length][2][2];
+            Vector2[][] newLines = new Vector2[lines.length][];
 
             for (int i = 0; i < lines.length; i++)
             {
                 int[][] line = lines[i];
+                newLines[i] = new Vector2[line.length];
 
-                for(int j=0; j<2; j++)
+                for(int j=0; j<line.length; j++)
                 {
                     int[] point = line[j];
                     int x = point[0];
                     int y = point[1];
 
-                    float newX = ((float) x)/size;
-                    float newY = ((float) y)/size;
-
-                    newLines[i][j][0] = newX;
-                    newLines[i][j][0] = newY;
+                    newLines[i][j] = new Vector2(((float) x)/size, ((float) -y)/size);
                 }
             }
 
             return newLines;
         }
 
-        public float[][] boundsToFloat()
+        public int getSize()
         {
-            float size = right - left;
-            float[][] newBbox = new float[2][2];
-            newBbox[0][0] = ((float) bbox[0][0])/size;
-            newBbox[0][1] = ((float) bbox[0][1])/size;
-            newBbox[1][0] = ((float) bbox[1][0])/size;
-            newBbox[1][1] = ((float) bbox[1][1])/size;
+            return (right - left)*2;
+        }
+
+        public Vector2[] boundsToVector()
+        {
+            float size = getSize();
+            Vector2[] newBbox = new Vector2[2];
+            newBbox[0] = new Vector2(((float) bbox[0][0])/size, ((float) bbox[0][1])/size);
+            newBbox[1] = new Vector2(((float) bbox[1][0])/size, ((float) bbox[1][1])/size);
             return newBbox;
         }
 
         public FloatGlyph toFloatGlyph()
         {
+            if (charcode == 503)
+            {
+                System.out.println("DLKSFJSDLKFJDSLKFJ");
+            }
             FloatGlyph newGlyph = new FloatGlyph();
-            newGlyph.bbox = boundsToFloat();
-            newGlyph.lines = linesToFloat();
+            newGlyph.bbox = boundsToVector();
+            newGlyph.lines = linesToVector();
             newGlyph.charcode = charcode;
+
+
             return newGlyph;
         }
     }
 
     private class FloatGlyph {
         int charcode; // Hershey (not ascii) character code
-        float[][] bbox; // glyph bounding box
-        float[][][] lines; // array of array of arrays (array of lines of points)
+        Vector2[] bbox; // glyph bounding box
+        Vector2[][] lines; // array of array of arrays (array of lines of points)
 
         public String toString()
         {
@@ -109,12 +112,13 @@ public class HersheyFont {
             FileReader fileReader = new FileReader("assets/fonts/hershey-occidental.json");
             Gson gson = new Gson();
             JsonReader jsonReader = new JsonReader(fileReader);
-            Glyph[] glyphs = gson.fromJson(jsonReader, Glyph[].class);
+            Glyph[] intGlyphs = gson.fromJson(jsonReader, Glyph[].class);
 
             this.glyphs = new HashMap<Integer, FloatGlyph>();
-            for (Glyph glyph : glyphs)
+            for (Glyph glyph : intGlyphs)
             {
-                this.glyphs.put(glyph.charcode, glyph.toFloatGlyph());
+                FloatGlyph floatGlyph = glyph.toFloatGlyph();
+                this.glyphs.put(glyph.charcode, floatGlyph);
             }
 
             fileReader.close();
@@ -156,7 +160,7 @@ public class HersheyFont {
         characterSets.put(SIMPLEX, simplexGlyphs);
     }
 
-    public float[][][] getCharacter(int keycode, CharacterSet characterSet)
+    public Vector2[][] getCharacter(int keycode, CharacterSet characterSet)
     {
         return characterSets.get(characterSet).get(keycode).lines;
     }
--- core/src/test/com/atc/simulator/Display/VectorText/TestGraphHersheyFont.java
+++ core/src/test/com/atc/simulator/Display/VectorText/TestGraphHersheyFont.java
@@ -3,9 +3,9 @@ package com.atc.simulator.Display.VectorText;
 import com.atc.simulator.vectors.GeographicCoordinate;
 import com.atc.simulator.vectors.GnomonicProjection;
 import com.atc.simulator.vectors.LineSolver;
-import org.knowm.xchart.QuickChart;
-import org.knowm.xchart.SwingWrapper;
-import org.knowm.xchart.XYChart;
+import com.atc.simulator.vectors.TestGraphLeastSquaresCircleSolver;
+import com.badlogic.gdx.math.Vector2;
+import org.knowm.xchart.*;
 import pythagoras.d.Line;
 import pythagoras.d.Vector3;
 
@@ -40,44 +40,67 @@ import java.util.ArrayList;
  * @author Luke Frisken
  */
 public class TestGraphHersheyFont {
-    public static void main(String[] arg)
-    {
-        HersheyFont font = new HersheyFont();
 
-        float[][][] lines = font.getCharacter(35, HersheyFont.CharacterSet.SIMPLEX);
+    public static class XYData{
+        public double[] xData;
+        public double[] yData;
 
-
-        double[] xData = new double[lines.length*2*2];
-        double[] yData = new double[lines.length*2*2];
-
-        int k = 0;
-        for (int i = 0; i < lines.length; i++)
+        public XYData(double[] xData, double[] yData)
         {
-            float[][] line = lines[i];
-
-            for(int j=0; j<2; j++)
-            {
-                float[] point = line[j];
-                float x = point[0];
-                float y = point[1];
-
-                xData[k] = x;
-                yData[k] = y;
-                k++;
-            }
+            this.xData = xData;
+            this.yData = yData;
         }
 
-        System.out.println("WHAT THE HELL");
+        public XYData(int size)
+        {
+            xData = new double[size];
+            yData = new double[size];
+        }
+    }
+
+
+    public static void main(String[] arg)
+    {
 
         // Create Chart
-        XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
-        chart.setWidth(1000);
-        chart.setHeight(1000);
+        XYChart chart = new XYChartBuilder().width(1000).height(1000).title("Test Graph Hershey Font").build();
+
+
+        HersheyFont font = new HersheyFont();
+
+        Vector2[][] lines = font.getCharacter(50, HersheyFont.CharacterSet.SIMPLEX);
+
+
+        XYSeries[] series = new XYSeries[lines.length];
+        for (int i = 0; i < lines.length; i++)
+        {
+            Vector2[] line = lines[i];
+            XYData data = dataFromVectorList(line);
+            series[i] = chart.addSeries("line " + i, data.xData, data.yData);
+            series[i].setXYSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line);
+        }
+
+        chart.getStyler().setXAxisMin(-0.5);
+        chart.getStyler().setXAxisMax(0.5);
+        chart.getStyler().setYAxisMin(-0.5);
+        chart.getStyler().setYAxisMax(0.5);
 
         // Show it
         new SwingWrapper(chart).displayChart();
 
 
+    }
+
+    public static XYData dataFromVectorList(Vector2[] vectors) {
+        XYData data = new XYData(vectors.length);
+
+        for (int i=0; i<vectors.length;i++)
+        {
+            data.xData[i] = vectors[i].x;
+            data.yData[i] = vectors[i].y;
+        }
+
+        return data;
     }
 }
 
