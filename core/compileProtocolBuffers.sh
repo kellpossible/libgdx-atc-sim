8f113ce6cde9e519ffa7888b8fff92673c419e42
Luke Frisken <l.frisken@gmail.com>
2016-10-05 01:03:48 +1100

    testing adsb input with python

--- core/compileProtocolBuffers.sh
+++ core/compileProtocolBuffers.sh
@@ -2,7 +2,10 @@
 echo "Compiling Protocol Buffers..."
 protoc -I=src/ --java_out=src/ src/main/proto/PredictionFeedServe.proto
 mv src/com/atc/simulator/ProtocolBuffers/PredictionFeedServe.java src/main/com/atc/simulator/ProtocolBuffers/PredictionFeedServe.java
-protoc -I=src/ --java_out=src/ src/main/proto/DebugDataFeedServe.proto
+mv python_src/main/proto/DebugDataFeedServe_pb2.py python_src/DebugDataFeedServe_pb2.py
+protoc -I=src/ --java_out=src/ --python_out=python_src/ src/main/proto/DebugDataFeedServe.proto
 mv src/com/atc/simulator/ProtocolBuffers/DebugDataFeedServe.java src/main/com/atc/simulator/ProtocolBuffers/DebugDataFeedServe.java
+sleep 0.1
 rm -rf src/com/
+rm -rf python_src/main
 echo "Finished Compiling Protocol Buffers"
---
+++ core/python_src/test_protobuf.py
@@ -0,0 +1,5 @@
+import DebugDataFeedServe_pb2 as NetworkInterfacePacket
+
+
+if __name__ == "__main__":
+    print("hello world")
